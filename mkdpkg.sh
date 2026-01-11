471e24fbbd54126b1e53ede4b7537a1fa458a50d
WerIstLuka <WerIstLuka@gmail.com>
2026-01-11 20:08:17 +0100

    fixed a lot of libadwaita stuff (only 1.5)

--- mkdpkg.sh
+++ mkdpkg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python3 generate-themes.py
+
+echo "building debian package"
 mkdir package
 cp -r usr/ package
 cp -r DEBIAN package
@@ -8,4 +10,5 @@ dpkg-deb --root-owner-group --build "package" > /dev/null
 mv package.deb 20.3-Mint-Y-Themes.deb
 rm -rf package
 
-tar -czvf 20.3-Mint-Y-Themes.tar.gz usr
+"making tar achive"
+tar -czf 20.3-Mint-Y-Themes.tar.gz usr
